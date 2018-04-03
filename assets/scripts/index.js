9e204adabe138a441b1dc79b367d1dd3f79910e3
Scott Thiessen <things@varyd.io>
2018-04-02 20:48:43 -0700

    Add initial homepage

---
+++ assets/scripts/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  var $sections  = $('section');
+
+  $sections.each(function(index) {
+
+    var $section  = $(this);
+
+    $section.find('.expand-collapse-controls a').click(function(e) {
+      e.preventDefault();
+      $section.toggleClass('collapsed');
+    });
+
+  });
+
+});
